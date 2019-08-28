780e4b1796787333ed95c0d45fb5a3fee5bcf5fd
arianacruz10 <arianaleonn@gmail.com>
2019-08-28 00:09:51 -0500

    estructura de datos en un programa

--- estructura.cpp
+++ estructura.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 struct persona{
-	char nombre[]
-};
+	char nombre[20];
+	int edad;
+}	persona1,persona2;
+
+int main(int argc,char** argv){
+	
+	cout<<"nombre: ";
+	cin.getline(persona2.nombre,20,'\n');
+	cout<<"edad: ";
+	cin>>persona2.edad;
+	cout<<persona2.nombre<<endl;
+	cout<<persona2.edad<<endl;
+	system("pause");
+	return 0;
+}
