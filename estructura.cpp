510be688839c4f6dcfadec69f2294774ecb9346c
arianacruz10 <arianaleonn@gmail.com>
2019-08-30 23:13:33 -0500

    version 2.0

--- estructura.cpp
+++ estructura.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 using namespace std;
 
-struct persona{
-	char nombre[20];
-	int edad;
-}	persona1,persona2;
+struct canciones{
+	char genero[20];
+	int anno;
+	char nombre_cancion[30];
 
+};
+struct artistas{
+	char nombre_vocalista[25];
+	int integrantes;
+	struct canciones dire;
+};	
 int main(int argc,char** argv){
+	struct artistas A1[5];
+	struct artistas *apuntador;
 	
-	cout<<"nombre: ";
-	cin.getline(persona2.nombre,20,'\n');
-	cout<<"edad: ";
-	cin>>persona2.edad;
-	cout<<persona2.nombre<<endl;
-	cout<<persona2.edad<<endl;
-	system("pause");
+	apuntador=A1;
+	
+	for(int i=0; i<2; i++){
+		cout<<"nombre de vocalista: ";
+		cin.getline(apuntador->nombre_vocalista,25, '\n');
+		cout<<"integrantes: ";
+		cin>>apuntador->integrantes;
+		cout<<"genero: ";
+		fflush(stdin);
+		cin.getline(apuntador->dire.genero,20, '\n');
+	}
+	
+	cout<<endl<<"nombre_vocalista guardado: "<<apuntador->nombre_vocalista;
+	cout<<endl<<"integrantes guardado: "<<apuntador->integrantes;
+	cout<<endl<<"genero: "<<apuntador->dire.genero;
+	
+	//getch();
 	return 0;
 }
